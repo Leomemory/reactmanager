13746696b217256cd5cc244060f7aaba628304f7
李江 <lijiang.0408@cashbus.com>
2019-12-17 15:44:26 +0800

    ui组件引入及使用

--- src/pages/ui/carousel.js
+++ src/pages/ui/carousel.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import {Card,Carousel} from 'antd'
+import './ui.less'
 
-class Carousel extends Component {
+class Carousels extends Component {
     constructor(props) {
         super(props);
         this.state = {  }
@@ -8,10 +10,30 @@ class Carousel extends Component {
     render() { 
         return (  
             <div>
-                carousel
+                 <Card title="文字背景轮播" className="card-wrap">
+                    <Carousel autoplay effect="fade">
+                        <div><h3>Ant Motion Banner - React</h3></div>
+                        <div><h3>Ant Motion Banner - Vue</h3></div>
+                        <div><h3>Ant Motion Banner - Angular</h3></div>
+                    </Carousel>
+                </Card>
+
+                <Card title="图片轮播" className="slider-wrap">
+                    <Carousel autoplay effect="fade">
+                        <div>
+                            <img src="/carousel-img/carousel-1.jpg" alt=""/>
+                        </div>
+                        <div>
+                            <img src="/carousel-img/carousel-2.jpg" alt="" />
+                        </div>
+                        <div>
+                            <img src="/carousel-img/carousel-3.jpg" alt="" />
+                        </div>
+                    </Carousel>
+                </Card>
             </div>
         );
     }
 }
  
-export default Carousel;
+export default Carousels;
--- src/pages/ui/loadings.js
+++ src/pages/ui/loadings.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Card, Spin, Icon, Alert} from  'antd'
+import './ui.less'
 
 class loadings extends Component {
     constructor(props) {
@@ -6,9 +8,48 @@ class loadings extends Component {
         this.state = {  }
     }
     render() { 
+        const icon = <Icon  type="loading" style={{fontSize:24}}/>
+        const iconLoading = <Icon type="loading" style={{ fontSize: 24 }} />
         return (  
             <div>
-                loadings
+                <Card title="Spin用法" className="card-wrap">
+                    <Spin size="small"/>
+                    <Spin style={{margin:'0 10px'}}/>
+                    <Spin size="large"/>
+                    <Spin indicator={icon} style={{ marginLeft: 10 }} spinning={true}/>
+                </Card>
+
+                <Card title="内容遮罩" className="card-wrap">
+                    <Alert
+                        message="React"
+                        description="欢迎来到React高级实战课程"
+                        type="info"
+                        style={{ marginBottom: 10 }}
+                    />
+                    <Spin>
+                        <Alert
+                            message="React"
+                            description="欢迎来到React高级实战课程"
+                            type="warning"
+                            style={{ marginBottom: 10 }}
+                        />
+                    </Spin>
+                    <Spin tip="加载中...">
+                        <Alert
+                            message="React"
+                            description="欢迎来到React高级实战课程"
+                            type="warning"
+                            style={{ marginBottom: 10 }}
+                        />
+                    </Spin>
+                    <Spin indicator={iconLoading}>
+                        <Alert
+                            message="React"
+                            description="欢迎来到React高级实战课程"
+                            type="warning"
+                        />
+                    </Spin>
+                </Card>
             </div>
         );
     }
